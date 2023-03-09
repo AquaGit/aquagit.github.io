a0c3e82b84fbacd859c53cba35d210aba15022d1
Aqua <areyouaqua@gmail.com>
2023-03-09 23:37:56 +0200

    fixmobileburger

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,29 @@
 import Header from "./components/Header/Header";
 import GetStarted from "./components/GetStarted/GetStarted";
-import ShopBy from "./components/ShopBy/ShopBy";
-import KeepingFurniture from "./components/KeepingFurniture/KeepingFurniture";
-import Main from "./components/Main/Main";
+// import ShopBy from "./components/ShopBy/ShopBy";
+// import KeepingFurniture from "./components/KeepingFurniture/KeepingFurniture";
+// import Main from "./components/Main/Main";
 // import Items from "./components/Items/Items";
-import Floyd from "./components/Floyd/Floyd";
-import Reviews from "./components/Reviews/Reviews";
-import Easy from "./components/Easy/Easy";
-import Prend from "./components/Prend/Prend";
-import Footer from "./components/Footer/Footer";
+// import Floyd from "./components/Floyd/Floyd";
+// import Reviews from "./components/Reviews/Reviews";
+// import Easy from "./components/Easy/Easy";
+// import Prend from "./components/Prend/Prend";
+// import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <>
       <Header />
       <GetStarted />
-      <ShopBy />
+      {/* <ShopBy />
       <KeepingFurniture />
-      <Main />
+      <Main /> */}
       {/* <Items /> */}
-      <Floyd />
+      {/* <Floyd />
       <Reviews/>
       <Easy />
       <Prend />
-      <Footer />
+      <Footer /> */}
     </>
   );
 };
